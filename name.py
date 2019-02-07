4e79d58e5debaea977946a3ba659f2ae9052ae67
dimasta4ka <kiselevich.dima@yandex.ru>
2019-02-08 01:38:28 +0300

    add concatenation

--- name.py
+++ name.py
@@ -2,3 +2,10 @@ name = "example string"
 print(name.title())
 print(name.upper())
 print(name.lower())
+first_name = "example"
+second_name = "string"
+full_name = first_name + " " + second_name
+print(full_name)
+print("Hello, " + full_name.title() + "!")
+massage = "Hello, " + full_name.title() + "!"
+print(massage)
