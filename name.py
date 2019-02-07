f359db4ef048a989e8c24109585f57cf476ad07a
dimasta4ka <kiselevich.dima@yandex.ru>
2019-02-08 02:32:09 +0300

    delimited tasks

--- name.py
+++ name.py
@@ -2,6 +2,8 @@ name = "example string"
 print(name.title())
 print(name.upper())
 print(name.lower())
+print(" ")
+
 first_name = "example"
 second_name = "string"
 full_name = first_name + " " + second_name
@@ -9,11 +11,16 @@ print(full_name)
 print("Hello, " + full_name.title() + "!")
 massage = "Hello, " + full_name.title() + "! "
 print(massage)
+print(" ")
+
 print("Python")
 print("\tPython")
 print("Languages:\nPython\nC\nJavaScript")
 print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+print(" ")
 massage = " Hello, " + full_name.title() + "! "
 print(massage.rstrip())
 print(massage.lstrip())
 print(massage.strip())
+
