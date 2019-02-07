e9fdaf30d056682e0d7454495d5edc66d4aac6ab
dimasta4ka <kiselevich.dima@yandex.ru>
2019-02-08 02:25:13 +0300

    add delet passes

--- name.py
+++ name.py
@@ -7,9 +7,13 @@ second_name = "string"
 full_name = first_name + " " + second_name
 print(full_name)
 print("Hello, " + full_name.title() + "!")
-massage = "Hello, " + full_name.title() + "!"
+massage = "Hello, " + full_name.title() + "! "
 print(massage)
 print("Python")
 print("\tPython")
 print("Languages:\nPython\nC\nJavaScript")
 print("Languages:\n\tPython\n\tC\n\tJavaScript")
+massage = " Hello, " + full_name.title() + "! "
+print(massage.rstrip())
+print(massage.lstrip())
+print(massage.strip())
