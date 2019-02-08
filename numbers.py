4e8e4dd99440133143e1879f6b11ede9e41fbbf0
dimasta4ka <kiselevich.dima@yandex.ru>
2019-02-08 03:20:21 +0300

    Studied numbers

---
+++ numbers.py
@@ -0,0 +1,9 @@
+print("Example")
+age = 23
+massage = "Happy " + str(age) +"rd Birthday!"
+# Without str() programm will not work.
+print(massage)
+print("\nTask")
+print(5+3, 4*2, 32/4, 15-7)
+sweats = 15
+print("I have "+ str(sweats) + " sweats")
