ba6b960e3b900b59d052152ee22cc70a19fc60d4
dimasta4ka <kiselevich.dima@yandex.ru>
2019-02-10 00:59:21 +0300

    started study lists

---
+++ lists.py
@@ -0,0 +1,9 @@
+soda = ['cola', 'fanta', 'sprite']
+print(soda[0])
+print(soda[1])
+print(soda[2])
+print('My favorite soda is ' + soda[0].title())
+print('My favorite soda is ' + soda[1].title())
+print('My favorite soda is ' + soda[2].title())
+
+
