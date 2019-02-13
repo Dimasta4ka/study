66683f04bc85fcdc9b52d459cc66f76717ab07fa
dimasta4ka <kiselevich.dima@yandex.ru>
2019-02-13 13:21:38 +0300

    sort

---
+++ cars.py
@@ -0,0 +1,20 @@
+cars = ['bmv', 'audi', 'toyota', 'subaru']
+cars.sort()
+print(cars)
+cars.sort(reverse=True)
+print(cars)
+
+cars = ['bmv', 'audi', 'toyota', 'subaru']
+print("\nhere is the original list:")
+print(cars)
+print("\nhere is the sorted list:")
+print(sorted(cars))
+print("\nhere is the original list again:")
+print(cars)
+
+cars = ['bmv', 'audi', 'toyota', 'subaru']
+cars.reverse()
+print(cars)
+
+cars = ['bmv', 'audi', 'toyota', 'subaru']
+len(cars)
