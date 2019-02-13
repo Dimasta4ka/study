9815f17da084b7fd256916c5f1a721a1a1a80c99
dimasta4ka <kiselevich.dima@yandex.ru>
2019-02-14 02:49:20 +0300

    creating numeric lists

--- numbers.py
+++ numbers.py
@@ -1,9 +1,24 @@
-print("Example")
-age = 23
-massage = "Happy " + str(age) +"rd Birthday!"
-# Without str() programm will not work.
-print(massage)
-print("\nTask")
-print(5+3, 4*2, 32/4, 15-7)
-sweats = 15
-print("I have "+ str(sweats) + " sweats")
+for value in range(1, 5):
+    print(value)
+number = list(range(1, 6))
+print(number)
+numbers = list(range(2, 11, 2))
+print(numbers)
+
+squares = []
+for value in range(1, 11):
+    square = value**2
+    squares.append(square)
+print(squares)
+#Можно записать тот же код более компактным способом(опустить переменную square)
+squares = []
+for value in range(1, 11):
+    squares.append(value**2)
+print(squares)
+
+print(min(squares))
+print(max(squares))
+print(sum(squares))
+
+squares = [value**2 for value in range(1, 11)]
+print(squares)
