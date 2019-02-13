e9acaff4c1258ce6298f2fe5c1ad837062aeabd3
dimasta4ka <kiselevich.dima@yandex.ru>
2019-02-14 02:21:11 +0300

    cycle for

---
+++ magicians.py
@@ -0,0 +1,5 @@
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician.title() + ", that was a great trick!")
+    print("I can't wait to see yournext trick, " + magician.title() + ".\n")
+print("Thank you, everone. That was a great magic show!")
