bcfc632c5c951068a4e9e59ee720b39878aba835
dimasta4ka <kiselevich.dima@yandex.ru>
2019-03-15 00:46:12 +0300

    tuples

---
+++ tuples.py
@@ -0,0 +1,7 @@
+menus = ('pizza', 'salat', 'meat', 'bread', 'water')
+for menu in menus:
+    print(menu)
+print("\n")
+menus = ('pasta', 'salat', 'meat', 'bread', 'juce')
+for menu in menus:
+    print(str(menu))
